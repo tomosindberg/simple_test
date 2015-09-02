2590985dc218c2948bae9a8e114141b774f48be5
Tomo Sindberg <tsindberg@gmail.com>
2015-09-01 21:45:19 -0700

    added watch/copy to ./bin to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var gulpMocha = require('gulp-mocha');
+var watch = require('gulp-watch');
+var incrementalBuild = 'test'; //changes showed up in ./bin/gulpfile.js
 
 gulp.task('jshint', function(){
   return gulp.src(['index.js', 'bin/greet', 'test/**/*.js', 'lib/**/*.js', 'gulpfile.js'])
@@ -13,4 +15,12 @@ gulp.task('test', function(){  //mocha/chai ./test/great_test.js
     .pipe(gulpMocha({reporter: 'nyan'}));
 });
 
-gulp.task('default', ['jshint', 'test']);
+gulp.task('stream', function(){  //copies files to ./build folder
+  return gulp.src(['index.js', 'bin/greet', 'test/**/*.js', 'lib/**/*.js', 'gulpfile.js'])
+    .pipe(watch(['index.js', 'bin/greet', 'test/**/*.js', 'lib/**/*.js', 'gulpfile.js']))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['jshint', 'test', 'stream']);
+
+
